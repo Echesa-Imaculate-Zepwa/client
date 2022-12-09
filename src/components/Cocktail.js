567e66d1841e90c7195bec8a81bde589a3eda285
Echesa-Imaculate-Zepwa <110254581+Echesa-Imaculate-Zepwa@users.noreply.github.com>
2022-12-09 16:59:04 +0300

    Update Cocktail.js

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -2,22 +2,23 @@ import React from "react";
 
 
 const Cocktail = ({cocktails}) => {
-    // console.log(products[0].price)
+    console.log(cocktails)
 
     return (
          <div className="card-container">
             <h1>OUR DRINKS</h1>
-        {cocktails.map(cocktail => {
+            <hr></hr>
+        {cocktails.map(cocktail => (
           <div className="card">
               <img src={cocktail.image} alt="our-drinks" className="card-image" /> 
               <h1>{cocktail.name}</h1>
               <p className="price">Price: {cocktail.price}</p>
               <p className="category">Category: {cocktail.category}</p>
-              <p className="image">{cocktail.image}</p>
-              {/* <p><button>Add to Cart</button></p> */}
+              <hr></hr>
+
           </div>
            
-    })}
+           ))}
         </div>
     );
 }
