06c7d33158a9adfc6f0be02b7fc2f6366fa121ab
Echesa-Imaculate-Zepwa <global-imaculate.zepwa@student.moringaschool.com>
2022-12-11 15:29:20 +0300

    edited the index.css

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
+// client/src/components/App.js
 import { useState, useEffect } from "react";
 import Cocktail from "./components/Cocktail";
 import SearchBar from './components/SearchBar';
 
 function App() {
   const [cocktails, setCocktails] = useState([]);
-  // const [loading, setLoading] = useState([])
+useEffect(() => {
+  fetch("https://cocktail-shop-production.up.railway.app/cocktails")
+  .then((response) => response.json())
+  .then((data) => setCocktails(data));
+},[])
 
-  // useEffect(() => {
-    // const fetchCocktailList = async () => {
-        // const baseUrl = "/cocktails";
-
-        // setLoading(true);
-
-        // try {
-            // const res = await fetch(`${baseUrl}filter.php?c=Cocktail`);
-            // const data = await res.json();
-            // console.log(data);
-
-            // setCocktails(data);
-            // setLoading(false);
-        // } catch (err) {
-            // console.log("Error fetching data");
-
-            // setLoading(false);
-        // }
-      // };
-
-    // fetchCocktailList();
-  // }, []);
-
-
-  // useEffect(() => {
-    // fetch("http:/localhost:3000/cocktails")
-    // .then((response) => response.json())
-    // .then((data) => setCocktails(data));
-  // },[])
-  
-
-  const fetchData = () => {
-    return fetch("/cocktails")
-      .then((response) => response.json())
-      .then((data) => setCocktails(data))
-  }
-  useEffect(() => {
-    fetchData();
-  },[])
   console.log(cocktails)
-
 
   return (
     <>
@@ -57,3 +22,4 @@ function App() {
 }
 
 export default App;
+
