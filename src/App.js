bc104f6090aa64e033e9217ee8b95b1bd06e8b7c
Echesa-Imaculate-Zepwa <global-imaculate.zepwa@student.moringaschool.com>
2022-12-12 15:29:57 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 // client/src/components/App.js
 import { useState, useEffect } from "react";
 import Cocktail from "./components/Cocktail";
-import SearchBar from './components/SearchBar';
+import Navbar from "./components/Navbar";
+//import SearchBar from './components/SearchBar';
 
 function App() {
-  const [cocktails, setCocktails] = useState([]);
-useEffect(() => {
-  fetch("https://cocktail-shop-production.up.railway.app/cocktails")
-  .then((response) => response.json())
-  .then((data) => setCocktails(data));
-},[])
-
-  console.log(cocktails)
-
   return (
     <>
-     <SearchBar cocktails={cocktails} setCocktails={setCocktails} />
-     <Cocktail cocktails={cocktails}/>
+      <Navbar />
+      <Cocktail />
     </>
   );
 }
 
 export default App;
-
